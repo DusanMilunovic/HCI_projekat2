90747c5e0a03e11cf420a0e77d1c1807daf49403
Nikola <nikolanemzi@gmail.com>
2019-05-29 22:03:57 +0200

    Added binding for types

--- emlekmu/AddMonument.xaml.cs
+++ emlekmu/AddMonument.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using emlekmu.models;
+using emlekmu.models.IO;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Type = emlekmu.models.Type;
 
 namespace emlekmu
 {
@@ -29,6 +32,23 @@ namespace emlekmu
                 PropertyChanged(this, new PropertyChangedEventArgs(name));
             }
         }
+
+
+
+
+
+
+
+
+        public ObservableCollection<Type> Types
+        {
+            get { return (ObservableCollection<Type>)GetValue(TypesProperty); }
+            set { SetValue(TypesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Types.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TypesProperty =
+            DependencyProperty.Register("Types", typeof(ObservableCollection<Type>), typeof(AddMonument), new PropertyMetadata(new ObservableCollection<Type>()));
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -50,9 +70,105 @@ namespace emlekmu
             }
         }
 
+        public ObservableCollection<ErasComboItem> eras;
+
+        public ObservableCollection<ErasComboItem> Eras
+        {
+            get
+            {
+                return eras;
+            }
+            set
+            {
+                if (value != eras)
+                {
+                    eras = value;
+                    OnPropertyChanged("Eras");
+                }
+            }
+        }
+
+        public ObservableCollection<TouristicStatusComboItem> touristic;
+
+        public ObservableCollection<TouristicStatusComboItem> Touristic
+        {
+            get
+            {
+                return touristic;
+            }
+            set
+            {
+                if (value != touristic)
+                {
+                    touristic = value;
+                    OnPropertyChanged("Touristic");
+                }
+            }
+        }
+
+
+
         public AddMonument()
         {
             InitializeComponent();
+            Root.DataContext = this;
+            this.eras = ErasComboItem.initializeEraList();
+            this.touristic = TouristicStatusComboItem.initializeTouristicList();
+            this.Monument = new Monument();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //debug button method
+            int a = 3;
+            a = a + 1;
+        }
+    }
+
+    public class TouristicStatusComboItem
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        public TouristicStatusComboItem(string Name, int Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        public static ObservableCollection<TouristicStatusComboItem> initializeTouristicList()
+        {
+            var retVal = new ObservableCollection<TouristicStatusComboItem>();
+            retVal.Add(new TouristicStatusComboItem("Exploited", 0));
+            retVal.Add(new TouristicStatusComboItem("Available", 1));
+            retVal.Add(new TouristicStatusComboItem("Unavailable", 2));
+            return retVal;
+        }
+    }
+
+    public class ErasComboItem
+    {
+        public string Name { get; set; }
+
+        public int Value { get; set; }
+
+        public ErasComboItem(string Name, int Value)
+        {
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        public static ObservableCollection<ErasComboItem> initializeEraList()
+        {
+            var retVal = new ObservableCollection<ErasComboItem>();
+            retVal.Add(new ErasComboItem("Paleolith", 0));
+            retVal.Add(new ErasComboItem("Neolithic", 1));
+            retVal.Add(new ErasComboItem("Ancient", 2));
+            retVal.Add(new ErasComboItem("Medieval", 3));
+            retVal.Add(new ErasComboItem("Renaissance", 4));
+            retVal.Add(new ErasComboItem("Modern", 5));
+            return retVal;
         }
     }
 }
--- emlekmu/MainContent.xaml.cs
+++ emlekmu/MainContent.xaml.cs
@@ -17,17 +17,20 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Type = emlekmu.models.Type;
+using Color = emlekmu.models.Color;
 
 namespace emlekmu
 {
     /// <summary>
     /// Interaction logic for MainContent.xaml
     /// </summary>
-    public partial class MainContent : UserControl, INotifyPropertyChanged
+    public partial class MainContent : UserControl
     {
+        #region Event listeners
+        #endregion
         #region Data
 
-        
+
 
         protected virtual void OnPropertyChanged(string name)
         {
@@ -40,7 +43,8 @@ namespace emlekmu
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ObservableCollection<Type> types;
-        public ObservableCollection<Type> Types {
+        public ObservableCollection<Type> Types
+        {
             get
             {
                 return types;
@@ -51,6 +55,72 @@ namespace emlekmu
                 {
                     types = value;
                     OnPropertyChanged("Types");
+                }
+            }
+        }
+
+        ObservableCollection<Monument> monuments;
+        public ObservableCollection<Monument> Monuments
+        {
+            get
+            {
+                return monuments;
+            }
+            set
+            {
+                if (value != monuments)
+                {
+                    monuments = value;
+                    OnPropertyChanged("Monuments");
+                }
+            }
+        }
+        ObservableCollection<Monument> searchedMonuments;
+        public ObservableCollection<Monument> SearchedMonuments
+        {
+            get
+            {
+                return searchedMonuments;
+            }
+            set
+            {
+                if (value != searchedMonuments)
+                {
+                    searchedMonuments = value;
+                    OnPropertyChanged("SearchedMonuments");
+                }
+            }
+        }
+        ObservableCollection<Monument> filteredMonuments;
+        public ObservableCollection<Monument> FilteredMonuments
+        {
+            get
+            {
+                return filteredMonuments;
+            }
+            set
+            {
+                if (value != filteredMonuments)
+                {
+                    filteredMonuments = value;
+                    OnPropertyChanged("FilteredMonuments");
+                }
+            }
+        }
+
+        ObservableCollection<Tag> tags;
+        public ObservableCollection<Tag> Tags
+        {
+            get
+            {
+                return tags;
+            }
+            set
+            {
+                if (value != tags)
+                {
+                    tags = value;
+                    OnPropertyChanged("Tags");
                 }
             }
         }
@@ -83,12 +153,7 @@ namespace emlekmu
         List<Tag> tags_f;
         #endregion
         #region Lists
-        ObservableCollection<Monument> monuments;
-            ObservableCollection<Monument> searchedMonuments;
-            ObservableCollection<Monument> filteredMonuments;
 
-            ObservableCollection<Tag> tags;
-        
         #endregion
         #region Monument
         public delegate Monument onAddMonument(Monument m);
@@ -108,7 +173,7 @@ namespace emlekmu
 
         Monument addMonument(Monument t)
         {
-            this.monuments.Add(t);
+            this.Monuments.Add(t);
             this.findMonuments(
                 this.id_s,
                 this.name_s,
@@ -138,11 +203,11 @@ namespace emlekmu
 
         Monument removeMonument(int id)
         {
-            foreach (var m in this.monuments)
+            foreach (var m in this.Monuments)
             {
                 if (m.Id == id)
                 {
-                    this.monuments.Remove(m);
+                    this.Monuments.Remove(m);
                     this.findMonuments(
                         this.id_s,
                         this.name_s,
@@ -175,9 +240,9 @@ namespace emlekmu
 
         Monument editMonument(Monument t)
         {
-            if (this.monuments.Remove(t))
+            if (this.Monuments.Remove(t))
             {
-                this.monuments.Add(t);
+                this.Monuments.Add(t);
                 this.findMonuments(
                     this.id_s,
                     this.name_s,
@@ -209,9 +274,9 @@ namespace emlekmu
 
         Monument findMonument(int id)
         {
-            foreach (var m in this.monuments)
+            foreach (var m in this.Monuments)
             {
-                if(m.Id == id)
+                if (m.Id == id)
                 {
                     return m;
                 }
@@ -244,12 +309,12 @@ namespace emlekmu
             this.min_income_s = min_income;
             this.max_income_s = max_income;
             this.tags_s = tags;
-            List<Monument> sMonuments = new List<Monument>(this.monuments);
-            foreach (var monument in this.monuments)
+            List<Monument> sMonuments = new List<Monument>(this.Monuments);
+            foreach (var monument in this.Monuments)
             {
                 if (id != -1)
                 {
-                    if(monument.Id != id)
+                    if (monument.Id != id)
                     {
                         sMonuments.Remove(monument);
                     }
@@ -265,7 +330,7 @@ namespace emlekmu
 
                 if (typeName != "")
                 {
-                    if(!monument.Type.Name.ToString().ToLower().Contains(typeName.ToLower()))
+                    if (!monument.Type.Name.ToString().ToLower().Contains(typeName.ToLower()))
                     {
                         sMonuments.Remove(monument);
                     }
@@ -301,7 +366,7 @@ namespace emlekmu
                         sMonuments.Remove(monument);
                     }
                 }
-                
+
                 if (populated != -1)
                 {
                     bool match = false;
@@ -323,7 +388,7 @@ namespace emlekmu
 
                 if (min_income != -1)
                 {
-                    if(monument.Income < min_income)
+                    if (monument.Income < min_income)
                     {
                         sMonuments.Remove(monument);
                     }
@@ -337,11 +402,11 @@ namespace emlekmu
                     }
 
                 }
-                
-                if (tags.Count != 0)
+
+                if (Tags.Count != 0)
                 {
                     bool match = false;
-                    foreach (var t in tags)
+                    foreach (var t in Tags)
                     {
                         if (monument.Tags.IndexOf(t) != -1)
                             match = true;
@@ -474,10 +539,10 @@ namespace emlekmu
 
                 }
 
-                if (tags.Count != 0)
+                if (Tags.Count != 0)
                 {
                     bool match = false;
-                    foreach (var t in tags)
+                    foreach (var t in Tags)
                     {
                         if (monument.Tags.IndexOf(t) != -1)
                             match = true;
@@ -507,17 +572,17 @@ namespace emlekmu
 
         Type addType(Type t)
         {
-            this.types.Add(t);
+            this.Types.Add(t);
             return t;
         }
 
         Type removeType(int id)
         {
-            foreach (var t in this.types)
+            foreach (var t in this.Types)
             {
                 if (t.Id == id)
                 {
-                    this.types.Remove(t);
+                    this.Types.Remove(t);
                     return t;
                 }
             }
@@ -526,9 +591,9 @@ namespace emlekmu
 
         Type editType(Type t)
         {
-            if (this.types.Remove(t))
+            if (this.Types.Remove(t))
             {
-                this.types.Add(t);
+                this.Types.Add(t);
                 return t;
             }
             return null;
@@ -536,7 +601,7 @@ namespace emlekmu
 
         Type findType(int id)
         {
-            foreach (var t in this.types)
+            foreach (var t in this.Types)
             {
                 if (t.Id == id)
                     return t;
@@ -601,8 +666,23 @@ namespace emlekmu
         #endregion
         public MainContent()
         {
+            Tags = new ObservableCollection<Tag>();
 
+            this.Tags.Add(new Tag("Good", new Color(0, 100, 166), "Very good tag"));
+            this.Tags.Add(new Tag("GRood", new Color(100, 0, 166), "Very grood tag"));
+            this.Tags.Add(new Tag("GRooden", new Color(100, 166, 0), "Very grooden tag"));
+            this.Tags.Add(new Tag("Good", new Color(100, 100, 166), "Even verier good tag"));
+            this.Tags.Add(new Tag("GRood", new Color(100, 100, 166), "Even verier grood tag"));
+            this.Tags.Add(new Tag("GRooden", new Color(100, 166, 100), "Even verier grooden tag"));
+            this.Tags.Add(new Tag("Good", new Color(45, 100, 166), "Even verier beste tag"));
+            this.Tags.Add(new Tag("GRood", new Color(130, 207, 166), "Even verier bestere tag"));
+            this.Tags.Add(new Tag("GRooden", new Color(114, 20, 35), "Even verier besterederen tag"));
+            this.Tags.Add(new Tag("Good", new Color(66, 100, 200), "Even verier more grood beste tag"));
+            this.Tags.Add(new Tag("GRood", new Color(70, 100, 50), "Even verier more grooder beste tag"));
+            this.Tags.Add(new Tag("GRooden", new Color(20, 30, 20), "Even verier more grooderen bestere tagEven verier more grooderen bestere tagEven verier more grooderen bestere tagEven verier more grooderen bestere tagEven verier more grooderen bestere tagEven verier more grooderen bestere tag"));
             InitializeComponent();
+
+
 
             Root.DataContext = this;
             // data initialization
