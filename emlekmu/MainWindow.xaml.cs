dbbf6f2adf80af5cfea010041060bad3579c3dca
Dusan Milunovic <dusanmilunovic17@gmail.com>
2019-06-04 00:58:34 +0200

    help done

--- emlekmu/MainWindow.xaml.cs
+++ emlekmu/MainWindow.xaml.cs
@@ -19,7 +19,6 @@ using System.Threading;
 using System.Windows.Controls.Primitives;
 using System.Diagnostics;
 
-
 namespace emlekmu
 {
     /// <summary>
@@ -221,7 +220,7 @@ namespace emlekmu
 
         private void HelpDocumentationCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
-            Process.Start(@"..\..\resources\Emlekmu.chm");
+            HelpProvider.ShowHelpTopic("Emlekmu");
         }
 
         private void DemonModeCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
